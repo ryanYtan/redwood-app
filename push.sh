19bb1c2e0d4cf3d325a694b6bf61c06bb6636ff8
ryanYtan <tanyuryan@gmail.com>
2023-09-22 01:15:07 +0800

    modify push.sh to push to docker hub

--- push.sh
+++ push.sh
@@ -1,3 +1,1 @@
-docker build -t ecr-repository .
-docker tag ecr-repository:latest $ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/ecr-repository:latest
-docker push $ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com/ecr-repository:latest
+docker push ryanty/redwood:latest
